eb90b2eab1bdfb7894af3ee321cba46ca1b6e333
Manu Ticusan <manuelticusan@yahoo.com>
2019-02-15 21:34:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,16 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |k,v|
+    if k.to_s == season
+      v.each do |contestant_hash|        
+        if contestant_hash[:status] == "Winner"
+          return contestant_hash[:name].split(' ').first
+        end
+      end
+      return "No Winner"
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
